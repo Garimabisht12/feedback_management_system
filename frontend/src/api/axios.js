1beb581b0a73100afb37ddbee507e78799ea2946
Garima <118539728+Garimabisht12@users.noreply.github.com>
2026-01-04 13:17:39 +0000

    add url of backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://bias-feedbackmanagementsystem-production-64b2.up.railway.app/api/"
 });
 
 export default instance;
