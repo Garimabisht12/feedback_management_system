9cbeaeb119ba13a47db7efe1ded8c6cefb6597ec
kori <garima1205bisht@gmail.com>
2026-09-02 13:09:00 +0000

    add client url in cors

--- feedback-backend/index.js
+++ feedback-backend/index.js
@@ -17,8 +17,9 @@ const uploadRoutes = require('./src/routes/uploadRoutes')
 const feedbackRoutes = require('./src/routes/feedbackRoutes');
 
 const app = express()
+
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: [process.env.CLIENT_URL ||'http://localhost:5173'],
     credentials: true
 }))
 
