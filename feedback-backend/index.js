83f59cd58e0ab19ddde268520f02ce741b92737e
kori <garima1205bisht@gmail.com>
2026-09-02 13:24:36 +0000

    update cors origin

--- feedback-backend/index.js
+++ feedback-backend/index.js
@@ -19,7 +19,7 @@ const feedbackRoutes = require('./src/routes/feedbackRoutes');
 const app = express()
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL ||'http://localhost:5173'],
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
     credentials: true
 }))
 
