e13b708db15ed80cfdf8e69f8a85c2463b312a1d
kori <garima1205bisht@gmail.com>
2026-09-03 02:50:20 +0000

    fix DB connection for production

--- feedback-backend/index.js
+++ feedback-backend/index.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // const subjectRoutes = require('./src/routes/studentRoutes');
 
+const loginRoutes = require("./src/routes/loginRoutes");
+const teacherRoutes = require("./src/routes/teacherRoutes");
+const subjectRoutes = require("./src/routes/subjectRoutes");
+const assignmentRoutes = require("./src/routes/assignmentRoutes");
+const studentRoutes = require("./src/routes/studentRoutes");
+const uploadRoutes = require("./src/routes/uploadRoutes");
 
-const loginRoutes = require('./src/routes/loginRoutes')
-const teacherRoutes = require('./src/routes/teacherRoutes')
-const subjectRoutes = require('./src/routes/subjectRoutes')
-const assignmentRoutes = require('./src/routes/assignmentRoutes')
-const studentRoutes = require('./src/routes/studentRoutes')
-const uploadRoutes = require('./src/routes/uploadRoutes')
+const feedbackRoutes = require("./src/routes/feedbackRoutes");
 
-const feedbackRoutes = require('./src/routes/feedbackRoutes');
+const app = express();
 
-const app = express()
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    credentials: true,
+  }),
+);
 
-app.use(cors({
-    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
-    credentials: true
-}))
-
-app.use(express.json())
+app.use(express.json());
 
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
-app.get('/', (req, res)=>{
-    res.send('Backend Working!')
-})
+app.get("/", (req, res) => {
+  res.send("Backend Working!");
+});
 
-app.use('/api', loginRoutes)
-app.use('/api/admin/teacher', teacherRoutes)
-app.use('/api/admin/subject', subjectRoutes)
-app.use('/api/admin/feedback', feedbackRoutes)
-app.use('/api/admin/assignment', assignmentRoutes)
-app.use('/api/admin', uploadRoutes)
-app.use('/api/student', studentRoutes)
-
+app.use("/api", loginRoutes);
+app.use("/api/admin/teacher", teacherRoutes);
+app.use("/api/admin/subject", subjectRoutes);
+app.use("/api/admin/feedback", feedbackRoutes);
+app.use("/api/admin/assignment", assignmentRoutes);
+app.use("/api/admin", uploadRoutes);
+app.use("/api/student", studentRoutes);
 
 // app.use('/test', loginRoutes)
 // app.use('/test/admin/teacher', teacherRoutes)
@@ -46,20 +46,19 @@ app.use('/api/student', studentRoutes)
 // app.use('/test/admin/assignment', assignmentRoutes)
 // app.use('/test/student', studentRoutes)
 
-
-if (process.env.NODE_ENV !== 'production') {
-
-const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("MONGODB successfully connected!!!")
-        app.listen(PORT, () => {
-            console.log(`server running on ${PORT}`);
-        })
-    })
-    .catch(err => {
-        console.log(`error connecting db: ${err}`)
-    })
-}
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MONGODB successfully connected!!!");
+    if (process.env.NODE_ENV !== "production") {
+      const PORT = process.env.PORT || 5000;
+      app.listen(PORT, () => {
+        console.log(`server running on ${PORT}`);
+      });
+    }
+  })
+  .catch((err) => {
+    console.log(`error connecting db: ${err}`);
+  });
 
 module.exports = app;
